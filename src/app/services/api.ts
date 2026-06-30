a3068a14ed3aaaa447a92a231e0b48addea1f5a0
Jaysemmm <cortiguerrajcempatrick@gmail.com>
2026-06-30 11:17:28 +0800

    Added Apollo Client for GraphQL

--- src/app/services/api.ts
+++ src/app/services/api.ts
@@ -1,26 +1,74 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, throwError } from 'rxjs';
-import { catchError } from 'rxjs/operators';
+import { Apollo, gql } from 'apollo-angular';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { environment } from '../../environments/environment';
+
+// GraphQL Queries and Mutations
+const GET_STUDENTS = gql`
+  query {
+    students {
+      id
+      name
+      email
+      year_level
+      department
+      age
+      birthday
+      contact_number
+    }
+  }
+`;
+
+const UPDATE_STUDENT = gql`
+  mutation UpdateStudent(
+    $id: ID!
+    $name: String
+    $email: String
+    $year_level: String
+    $department: String
+    $age: Int
+    $birthday: String
+    $contact_number: String
+  ) {
+    updateStudent(
+      id: $id
+      name: $name
+      email: $email
+      year_level: $year_level
+      department: $department
+      age: $age
+      birthday: $birthday
+      contact_number: $contact_number
+    ) {
+      id
+      name
+      email
+    }
+  }
+`;
+
+const DELETE_STUDENT = gql`
+  mutation DeleteStudent($id: ID!) {
+    deleteStudent(id: $id) {
+      id
+    }
+  }
+`;
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
-
-
   private baseUrl = environment.apiUrl;
 
-  constructor(private http: HttpClient) {}
+  constructor(
+    private http: HttpClient,  // Keep for REST
+    private apollo: Apollo      //  Add for GraphQL
+  ) {}
 
-
-  private handleError(error: any): Observable<never> {
-    console.error('API Error:', error);
-    return throwError(() => error);
-  }
-
-
+  // REST - Register (stays as REST)
   register(data: {
     name: string;
     email: string;
@@ -30,25 +78,30 @@ export class ApiService {
     birthday: string;
     contact_number: string;
   }): Observable<any> {
-    return this.http.post(`${this.baseUrl}/register`, data)
-      .pipe(catchError(this.handleError));
+    return this.http.post(`${this.baseUrl}/register`, data);
   }
 
-
+  //  GraphQL - Get all students
   getStudents(): Observable<any[]> {
-    return this.http.get<any[]>(`${this.baseUrl}/students`)
-      .pipe(catchError(this.handleError));
+    return this.apollo.watchQuery({ query: GET_STUDENTS })
+      .valueChanges.pipe(
+        map((result: any) => result.data.students)
+      );
   }
 
-
+  // GraphQL - Update student
   updateStudent(id: number, data: any): Observable<any> {
-    return this.http.put(`${this.baseUrl}/students/${id}`, data)
-      .pipe(catchError(this.handleError));
+    return this.apollo.mutate({
+      mutation: UPDATE_STUDENT,
+      variables: { id, ...data }
+    });
   }
 
-
+  //  GraphQL - Delete student
   deleteStudent(id: number): Observable<any> {
-    return this.http.delete(`${this.baseUrl}/students/${id}`)
-      .pipe(catchError(this.handleError));
+    return this.apollo.mutate({
+      mutation: DELETE_STUDENT,
+      variables: { id }
+    });
   }
 }
